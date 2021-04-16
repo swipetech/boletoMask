62a6a9246235f680a04055dd3815d36a09ad2c4a
Joseildo Filho <filhojoseildo@gmail.com>
2021-04-16 17:28:22 -0300

    adding more tests

--- test/boleto_mask_test.dart
+++ test/boleto_mask_test.dart
@@ -44,5 +44,28 @@ void main() {
       boletoMask.formatEditUpdate(oldValue, newValue);
       expect(boletoMask.getMaskedText(), barCode);
     });
+
+    test(
+        'should apply mask that treats when the barcode starts with 8 and then changes to other value',
+        () {
+      var barCode = '82630000001 3 03440010053 2 02260742001 9 11202030003 7';
+      var barCodeUnmasked = '826300000013034400100532022607420019112020300037';
+      var oldValue = TextEditingValue(text: barCodeUnmasked);
+      var newValue = TextEditingValue(text: barCodeUnmasked);
+      boletoMask.formatEditUpdate(oldValue, newValue);
+      expect(boletoMask.getMaskedText(), barCode);
+
+      oldValue = TextEditingValue(text: '');
+      newValue = TextEditingValue(text: '');
+      boletoMask.formatEditUpdate(oldValue, newValue);
+
+      barCode = '23792.63201 40000.003935 40009.508009 1 859500000104';
+      barCodeUnmasked = '237926320140000003935400095080091859500000104';
+      oldValue = TextEditingValue(text: barCodeUnmasked);
+      newValue = TextEditingValue(text: barCodeUnmasked);
+      boletoMask.formatEditUpdate(oldValue, newValue);
+      print(boletoMask.getMask());
+      expect(boletoMask.getMaskedText(), barCode);
+    });
   });
 }
